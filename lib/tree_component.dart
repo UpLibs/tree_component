c5890ec3016a2b49b9ecebd393c17887fd6202e3
Marcos B. L. <marcos.benatti@uppoints.com>
2015-01-27 18:11:29 -0200

    class Tree and TreeNode

--- lib/tree_component.dart
+++ lib/tree_component.dart
@@ -1,1 +1,45 @@
 library tree_component;
+
+class Tree {
+ 
+}
+
+class TreeNode {
+//  String type;
+  List data;
+  List<TreeNode> _children = [];
+  TreeNode _parent;
+  bool expanded = false;
+  bool isParent = false;
+  
+  TreeNode(this.data , TreeNode parent) {
+    if (parent != null) {
+      this.parent = parent;
+      parent.isParent = true;
+    }
+  }
+  
+  List<TreeNode> get children => _children;
+  
+  TreeNode get parent => _parent;
+  
+  set parent(TreeNode parent) {
+    _parent = parent;
+    parent.isParent = true;
+    
+    if (!parent.children.contains(this)) {
+      parent.children.add(this);
+    }
+  }
+  
+  Map<String, Object> toJson() => {"data": data};
+  
+}
+
+abstract class TreeNodeListener extends TreeNode{
+  
+  TreeNodeListener(List data, TreeNode parent) : super(data, parent);
+  
+  onClickListenerAction();
+  
+}

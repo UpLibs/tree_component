2cd2cd4bc0b9299d3247d6a70123b46e8590b356
Marcos B. L. <marcos.benatti@uppoints.com>
2015-01-30 14:55:32 -0200

    margin

--- packages/tree_component/tree_component.dart
+++ packages/tree_component/tree_component.dart
@@ -9,23 +9,29 @@ class TreeComponent {
   TreeComponent(this._root);
 
   Element _parentElement;
+  
+  int _margin ;
 
-  void buildAt(Element parentElement,[int margin = 0]) {
+  static const int DEFAULT_MARGIN = 16 ;
+  
+  void buildAt(Element parentElement,[int margin = DEFAULT_MARGIN]) {
     if (this._parentElement != null) remove();
 
     this._parentElement = parentElement;
+    this._margin = margin != null ? margin : DEFAULT_MARGIN ;
 
-    _buildNode(parentElement, _root , margin);
+    _buildNode(parentElement, _root);
   }
+  
+  int get margin => _margin ;
+  set margin(int size) => _margin = size != null ? ( size >= 0 ? size : 0 ) : DEFAULT_MARGIN ; 
 
-  void _buildNode(Element parentElement, TreeNode node,[int margin]) {
-
-    if(margin == null) {
-      margin = -10;
-    }
+  void _buildNode(Element parentElement, TreeNode node) {
     UListElement elem = new UListElement();
     elem.style.listStyleType = 'none';
-    elem.style.marginLeft = margin.toString()+"px";
+    elem.style.marginLeft = "10px";
+    
+    elem.style.paddingLeft = "${_margin}px";
 
     node._treeElement = elem;
 
@@ -81,7 +87,26 @@ class TreeComponent {
 
 
 class TreeNodeComponent {
-
+  
+  static String _IMG_ARROW_RIGHT =  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAAg0lEQVQ4jcXPrQ0CQRRF4Y8fh0LRwVIENVALLdAEDdAFBrsSj1sawIJhMWxCXibhZRB7kytGnDP3MXZm4X1FgxaPGmH/6R27wgdpwdALNv8IerxwxKpWEM+a1wqGnr+BaWZWyKR2wQ3bX/YS+MQei8y8CJ+wzoBR0GXmltLhgGUNPE7e4DU8I8utB5cAAAAASUVORK5CYII=" ;
+  static String _IMG_ARROW_DOWN = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAAlwSFl"+
+                "zAAALEwAACxMBAJqcGAAABCRpVFh0WE1MOmNvbS5hZG9iZS54bXAAAAAAADx4OnhtcG1ldGEgeG1sbnM6eD0iYWRvYmU6bnM6bWV0YS8iIHg6"+
+                "eG1wdGs9IlhNUCBDb3JlIDUuNC4wIj4KICAgPHJkZjpSREYgeG1sbnM6cmRmPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5LzAyLzIyLXJkZi1ze"+
+                "W50YXgtbnMjIj4KICAgICAgPHJkZjpEZXNjcmlwdGlvbiByZGY6YWJvdXQ9IiIKICAgICAgICAgICAgeG1sbnM6dGlmZj0iaHR0cDovL25zLm"+
+                "Fkb2JlLmNvbS90aWZmLzEuMC8iCiAgICAgICAgICAgIHhtbG5zOmV4aWY9Imh0dHA6Ly9ucy5hZG9iZS5jb20vZXhpZi8xLjAvIgogICAgICA"+
+                "gICAgICB4bWxuczpkYz0iaHR0cDovL3B1cmwub3JnL2RjL2VsZW1lbnRzLzEuMS8iCiAgICAgICAgICAgIHhtbG5zOnhtcD0iaHR0cDovL25z"+
+                "LmFkb2JlLmNvbS94YXAvMS4wLyI+CiAgICAgICAgIDx0aWZmOlJlc29sdXRpb25Vbml0PjI8L3RpZmY6UmVzb2x1dGlvblVuaXQ+CiAgICAgI"+
+                "CAgIDx0aWZmOkNvbXByZXNzaW9uPjU8L3RpZmY6Q29tcHJlc3Npb24+CiAgICAgICAgIDx0aWZmOlhSZXNvbHV0aW9uPjcyPC90aWZmOlhSZX"+
+                "NvbHV0aW9uPgogICAgICAgICA8dGlmZjpPcmllbnRhdGlvbj4xPC90aWZmOk9yaWVudGF0aW9uPgogICAgICAgICA8dGlmZjpZUmVzb2x1dGl"+
+                "vbj43MjwvdGlmZjpZUmVzb2x1dGlvbj4KICAgICAgICAgPGV4aWY6UGl4ZWxYRGltZW5zaW9uPjE2PC9leGlmOlBpeGVsWERpbWVuc2lvbj4K"+
+                "ICAgICAgICAgPGV4aWY6Q29sb3JTcGFjZT4xPC9leGlmOkNvbG9yU3BhY2U+CiAgICAgICAgIDxleGlmOlBpeGVsWURpbWVuc2lvbj4xNjwvZ"+
+                "XhpZjpQaXhlbFlEaW1lbnNpb24+CiAgICAgICAgIDxkYzpzdWJqZWN0PgogICAgICAgICAgICA8cmRmOkJhZy8+CiAgICAgICAgIDwvZGM6c3"+
+                "ViamVjdD4KICAgICAgICAgPHhtcDpNb2RpZnlEYXRlPjIwMTUtMDEtMjhUMTU6MDE6MzA8L3htcDpNb2RpZnlEYXRlPgogICAgICAgICA8eG1"+
+                "wOkNyZWF0b3JUb29sPlBpeGVsbWF0b3IgMy4zLjE8L3htcDpDcmVhdG9yVG9vbD4KICAgICAgPC9yZGY6RGVzY3JpcHRpb24+CiAgIDwvcmRm"+
+                "OlJERj4KPC94OnhtcG1ldGE+CjaK2CoAAACDSURBVDgRY2AYVuAh0Df/icS3YD5ngjGA9BYkNiHmTmwKBIGCxLjiPVCdADYDQGK+QEzIG/m4N"+
+                "MPEQc7DZch5oBwzTCEuWh0o8ROI0Q35BxSzxqUJXbwBKIBuwCJ0Rfj43EDJR0iGgAJOHJ8GbHLIAUow4LAZABLbD8T7gJgFxBmmAAAP/DrikG"+
+                "TL6wAAAABJRU5ErkJggg==" ;
+  
   TreeComponent _treeComponent;
 
   TreeNode _node;
@@ -119,35 +144,11 @@ class TreeNodeComponent {
     });
 
     if (_node.hasChildren) {
-
       ImageElement imgArrow = new ImageElement()
-      ..src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAAg0lEQVQ"+
-              "4jcXPrQ0CQRRF4Y8fh0LRwVIENVALLdAEDdAFBrsSj1sawIJhMWxCXibhZRB7kytGnDP3MXZm4X1FgxaPGmH/6R27wgdpwdALNv8I"+
-              "erxwxKpWEM+a1wqGnr+BaWZWyKR2wQ3bX/YS+MQei8y8CJ+wzoBR0GXmltLhgGUNPE7e4DU8I8utB5cAAAAASUVORK5CYII=";
-
-      if (_node.isExpanded) {
-
-        imgArrow.src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAAlwSFl"+
-                "zAAALEwAACxMBAJqcGAAABCRpVFh0WE1MOmNvbS5hZG9iZS54bXAAAAAAADx4OnhtcG1ldGEgeG1sbnM6eD0iYWRvYmU6bnM6bWV0YS8iIHg6"+
-                "eG1wdGs9IlhNUCBDb3JlIDUuNC4wIj4KICAgPHJkZjpSREYgeG1sbnM6cmRmPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5LzAyLzIyLXJkZi1ze"+
-                "W50YXgtbnMjIj4KICAgICAgPHJkZjpEZXNjcmlwdGlvbiByZGY6YWJvdXQ9IiIKICAgICAgICAgICAgeG1sbnM6dGlmZj0iaHR0cDovL25zLm"+
-                "Fkb2JlLmNvbS90aWZmLzEuMC8iCiAgICAgICAgICAgIHhtbG5zOmV4aWY9Imh0dHA6Ly9ucy5hZG9iZS5jb20vZXhpZi8xLjAvIgogICAgICA"+
-                "gICAgICB4bWxuczpkYz0iaHR0cDovL3B1cmwub3JnL2RjL2VsZW1lbnRzLzEuMS8iCiAgICAgICAgICAgIHhtbG5zOnhtcD0iaHR0cDovL25z"+
-                "LmFkb2JlLmNvbS94YXAvMS4wLyI+CiAgICAgICAgIDx0aWZmOlJlc29sdXRpb25Vbml0PjI8L3RpZmY6UmVzb2x1dGlvblVuaXQ+CiAgICAgI"+
-                "CAgIDx0aWZmOkNvbXByZXNzaW9uPjU8L3RpZmY6Q29tcHJlc3Npb24+CiAgICAgICAgIDx0aWZmOlhSZXNvbHV0aW9uPjcyPC90aWZmOlhSZX"+
-                "NvbHV0aW9uPgogICAgICAgICA8dGlmZjpPcmllbnRhdGlvbj4xPC90aWZmOk9yaWVudGF0aW9uPgogICAgICAgICA8dGlmZjpZUmVzb2x1dGl"+
-                "vbj43MjwvdGlmZjpZUmVzb2x1dGlvbj4KICAgICAgICAgPGV4aWY6UGl4ZWxYRGltZW5zaW9uPjE2PC9leGlmOlBpeGVsWERpbWVuc2lvbj4K"+
-                "ICAgICAgICAgPGV4aWY6Q29sb3JTcGFjZT4xPC9leGlmOkNvbG9yU3BhY2U+CiAgICAgICAgIDxleGlmOlBpeGVsWURpbWVuc2lvbj4xNjwvZ"+
-                "XhpZjpQaXhlbFlEaW1lbnNpb24+CiAgICAgICAgIDxkYzpzdWJqZWN0PgogICAgICAgICAgICA8cmRmOkJhZy8+CiAgICAgICAgIDwvZGM6c3"+
-                "ViamVjdD4KICAgICAgICAgPHhtcDpNb2RpZnlEYXRlPjIwMTUtMDEtMjhUMTU6MDE6MzA8L3htcDpNb2RpZnlEYXRlPgogICAgICAgICA8eG1"+
-                "wOkNyZWF0b3JUb29sPlBpeGVsbWF0b3IgMy4zLjE8L3htcDpDcmVhdG9yVG9vbD4KICAgICAgPC9yZGY6RGVzY3JpcHRpb24+CiAgIDwvcmRm"+
-                "OlJERj4KPC94OnhtcG1ldGE+CjaK2CoAAACDSURBVDgRY2AYVuAh0Df/icS3YD5ngjGA9BYkNiHmTmwKBIGCxLjiPVCdADYDQGK+QEzIG/m4N"+
-                "MPEQc7DZch5oBwzTCEuWh0o8ROI0Q35BxSzxqUJXbwBKIBuwCJ0Rfj43EDJR0iGgAJOHJ8GbHLIAUow4LAZABLbD8T7gJgFxBmmAAAP/DrikG"+
-                "TL6wAAAABJRU5ErkJggg==";
-      }  
-        _element.children.add(imgArrow);
+      ..src = ( _node.isExpanded ? _IMG_ARROW_DOWN : _IMG_ARROW_RIGHT) ;
       
-
+      _element.children.add(imgArrow);
+      
       imgArrow.onClick.listen((L) {
         if (_node.isExpanded) {
           _node.expanded = false;
@@ -162,7 +163,9 @@ class TreeNodeComponent {
         if(_node.listener != null)
         _node.listener.onExpandAction(_node);
       });
-
+    }
+    else {
+      
     }
 
     SpanElement spanElementName = new SpanElement()
@@ -172,7 +175,6 @@ class TreeNodeComponent {
       if(_node.listener != null)
       _node.listener.onClickAction(_node);
     });
-    
 
     if(_node.properties['color']!=null){
       LabelElement colorLabel = new LabelElement()
@@ -186,6 +188,7 @@ class TreeNodeComponent {
       ;
       _element.children.add(colorLabel);
     }
+    
     _element.children.add(checkBox);
     _element.children.add(spanElementName); //TODO adicionar listener aqui!
 
